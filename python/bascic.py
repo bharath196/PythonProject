ecf5591b970282540e762f4996fe283897995b0e
Bharath Kumar Reddy <bharath984951@gmail.com>
2024-10-13 23:10:49 +0530

    Initial commit

---
+++ python/bascic.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = []
+d = []
+for i in range(2,a):
+    c = 0
+    for j in range(2,i):
+        if(i%j == 0):
+            c = c + 1
+    if(c >= 1):
+        print(i,"is a not a prime number")
+        b.append(i)
+    else:
+        print(i,"is a prime numbeer")
+        d.append(i)  
+
+print(d," are prime numbers",b,"---are not prime numbers-----")              
